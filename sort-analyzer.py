2d4fd4bba0f8f3dc9e04c27ebbc9f62e357cab2c
ChauBui00 <c.bui3@share.epsb.ca>
2023-11-27 13:30:11 -0700

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -25,9 +25,9 @@ fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
 
 # VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
 print(randomData[0:50])
-print(reversedData[0:50])
-print(nearlySortedData[0:50])
-print(fewUniqueData[0:50])
+# print(reversedData[0:50])
+# print(nearlySortedData[0:50])
+# print(fewUniqueData[0:50])
 
 
 # EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
@@ -35,3 +35,23 @@ print(fewUniqueData[0:50])
 # bubbleSort(randomData)
 # endTime = time.time()
 # print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
+
+startTime = time.time()
+def bubbleSort(anArray):
+    for numCompare in range(len(anArray)-1, 0, -1):
+        for i in range(numCompare):
+           
+            firstNum = anArray[i]
+            secNum = anArray[i+1]
+
+
+            if firstNum > secNum:
+                anArray[i] = secNum
+                anArray[i + 1] = firstNum
+bubbleSort(randomData)
+endTime = time.time()
+print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
+
+import math
+result = (4.811554431915283 + 4.717653512954712 + 5.5849692821502686)/3
+print(result)
