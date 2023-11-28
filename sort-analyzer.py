d53a2d19dfc43b5f300b9c1e2bc4749d220ee4da
ChauBui00 <c.bui3@share.epsb.ca>
2023-11-28 13:29:20 -0700

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -24,7 +24,7 @@ nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt")
 fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
 
 # VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
-print(randomData[0:50])
+# print(randomData[0:50])
 # print(reversedData[0:50])
 # print(nearlySortedData[0:50])
 # print(fewUniqueData[0:50])
@@ -36,22 +36,22 @@ print(randomData[0:50])
 # endTime = time.time()
 # print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
 
-startTime = time.time()
-def bubbleSort(anArray):
-    for numCompare in range(len(anArray)-1, 0, -1):
-        for i in range(numCompare):
-           
-            firstNum = anArray[i]
-            secNum = anArray[i+1]
 
+# startTime = time.time()
+# def selectionSort(anArray):
+#     for fillSlot in range(len(anArray) - 1):
+#         # Find position of minimum element
+#         minPosition = fillSlot
+#         for i in range(fillSlot + 1, len(anArray)):
+#             if anArray[i] < anArray[minPosition]:
+#                 minPosition = i
+#         # Swap fillSot and the minPosition
+#         anArray[fillSlot], anArray[minPosition] = anArray[minPosition], anArray[fillSlot]
+# selectionSort(reversedData)
+# endTime = time.time()
+# total_Time= endTime - startTime
+# print(f"Selection Sort Random Data: {total_Time} seconds")
 
-            if firstNum > secNum:
-                anArray[i] = secNum
-                anArray[i + 1] = firstNum
-bubbleSort(randomData)
-endTime = time.time()
-print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
+result = (2.4880409240722656 +  2.4667487144470215 + 2.487828016281128)/3
 
-import math
-result = (4.811554431915283 + 4.717653512954712 + 5.5849692821502686)/3
-print(result)
+print(f"\nThe average time is: {result} seconds")
